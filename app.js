762e92f62ca1ca24555cb68d53c5932ce9e70e88
Felix E. Klee <felix.klee@inka.de>
2016-12-12 12:51:01 +0100

    Remove unneeded server code

--- app.js
+++ app.js
@@ -7,23 +7,13 @@
 var express = require("express");
 var app = express();
 var httpServer = require("http").createServer(app);
-var wsConnections = require("./app/server/ws_connections");
-var boards = require("./app/server/boards");
 var routes = require("./routes/create")(app.get("env"));
-var startHttpServer;
-var loadBoardsAndStartServer;
 
-startHttpServer = function () {
+var startHttpServer = function () {
     httpServer.listen(app.get("port"), function () {
         console.log("Express server listening on port %d in %s mode",
                 app.get("port"), app.settings.env);
     });
-
-    wsConnections.startServer(httpServer);
-};
-
-loadBoardsAndStartServer = function () {
-    boards.load(startHttpServer);
 };
 
 app.set("port", process.env.PORT || 3000);
@@ -46,8 +36,8 @@ app.get("/web-app-manifest.json", routes.webAppManifestJson);
 if (app.get("env") === "development") {
     app.use("/app", express.static(__dirname + "/app"));
     app.use(express.errorHandler());
-    loadBoardsAndStartServer();
+    startHttpServer();
 } else { // production
     app.use("/app.build", express.static(__dirname + "/app.build"));
-    require("./app/server/optimize")(loadBoardsAndStartServer);
+    require("./app/server/optimize")(startHttpServer);
 }
